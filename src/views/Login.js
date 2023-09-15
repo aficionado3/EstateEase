c7c5dc0eefaec16582b0889fdc987eb5a98144b6
Agnes Koinange <koinangeagness@gmail.com>
2023-09-15 09:10:18 +0300

    ft: style login and properties

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,15 +1,87 @@
+// import React, { useState } from 'react';
+// import Web3 from 'web3';
+// import '../styles/Login.css';
+
+// function Login() {
+//     const [username, setUsername] = useState('');
+//     const [password, setPassword] = useState('');
+//     const [account, setAccount] = useState('');
+//     const [userRole, setUserRole] = useState(''); // New State for UserRole
+
+//     const handleLogin = () => {
+//         console.log('Logged in as', username, 'with role', userRole);
+//     };
+
+//     const loginWithMetaMask = async () => {
+//         if (window.ethereum) {
+//             const web3 = new Web3(window.ethereum);
+//             try {
+//                 // Request account access
+//                 const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+//                 setAccount(accounts[0]);
+//                 console.log('Logged in with MetaMask:', accounts[0]);
+//             } catch (error) {
+//                 console.error('User denied account access');
+//             }
+//         } else {
+//             console.error('Ethereum browser not detected. Try installing MetaMask.');
+//         }
+//     };
+
+//     return (
+//         <div className="login-container">
+//             <h2>Login</h2>
+
+//             <select
+//     value={userRole}
+//     onChange={(e) => setUserRole(e.target.value)}
+//     className="role-select"
+// >
+//     <option value="" disabled>Select your role</option>
+//     <option value="PropertyOwner">Property Owner</option>
+//     <option value="ClientBuyer">Client/Buyer</option>
+// </select>
+
+
+//             <input 
+//                 type="text" 
+//                 placeholder="Username" 
+//                 value={username} 
+//                 onChange={(e) => setUsername(e.target.value)}
+//             />
+//             <input 
+//                 type="password" 
+//                 placeholder="Password" 
+//                 value={password} 
+//                 onChange={(e) => setPassword(e.target.value)}
+//             />
+//             <button onClick={handleLogin}>Login</button>
+            
+//             <div>
+//                 <h3>OR</h3>
+//                 <button onClick={loginWithMetaMask}>Login with MetaMask</button>
+//             </div>
+//         </div>
+//     );
+// }
+
+// export default Login;
+
+
 import React, { useState } from 'react';
 import Web3 from 'web3';
 import '../styles/Login.css';
+import { useNavigate } from 'react-router-dom';
 
 function Login() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [account, setAccount] = useState('');
-    const [userRole, setUserRole] = useState(''); // New State for UserRole
+    const navigate = useNavigate();
 
     const handleLogin = () => {
-        console.log('Logged in as', username, 'with role', userRole);
+        console.log('Logged in as', username);
+        navigate('/properties');
     };
 
     const loginWithMetaMask = async () => {
@@ -26,23 +98,15 @@ function Login() {
         } else {
             console.error('Ethereum browser not detected. Try installing MetaMask.');
         }
+        navigate('/properties');
     };
 
-    return (
-        <div className="login-container">
+    // ... (Other parts of your code)
+
+return (
+    <div className="login-container">
+        <div className="form-box">
             <h2>Login</h2>
-
-            <select
-    value={userRole}
-    onChange={(e) => setUserRole(e.target.value)}
-    className="role-select"
->
-    <option value="" disabled>Select your role</option>
-    <option value="PropertyOwner">Property Owner</option>
-    <option value="ClientBuyer">Client/Buyer</option>
-</select>
-
-
             <input 
                 type="text" 
                 placeholder="Username" 
@@ -62,7 +126,9 @@ function Login() {
                 <button onClick={loginWithMetaMask}>Login with MetaMask</button>
             </div>
         </div>
-    );
+    </div>
+);
+
 }
 
 export default Login;
--- src/views/PropertyListing.js
+++ src/views/PropertyListing.js
@@ -40,6 +40,7 @@
 import React, { useState } from 'react';
 import '../styles/PropertyStyles.css';
 import properties from '../assets/properties';
+import { Link } from 'react-router-dom';
 
 function PropertyListing({ type }) {
     const placeholderImage = 'https://images.pexels.com/photos/280229/pexels-photo-280229.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500';
@@ -57,49 +58,64 @@ function PropertyListing({ type }) {
         .filter(property => property.title.toLowerCase().includes(searchTerm.toLowerCase()));
 
     return (
-        <div className="property-container">
-            <div className="search-container">
-                <input
-                    type="text"
-                    placeholder={`Search ${type} properties by title...`}
-                    value={searchTerm}
-                    onChange={(e) => setSearchTerm(e.target.value)}
-                />
-            </div>
-            {filteredProperties.map((property) => (
-                <div className="property-card" key={property.id}>
-                    <img 
-                        src={property.image} 
-                        alt={property.title} 
-                        className="property-image"
-                        onError={handleImageError}
-                    />
-                    <h3 className="property-title">{property.title}</h3>
-                    {showDetailsProperty === property.id ? (
-                        <div>
-                            <p className="property-description">{property.description}</p>
-                            <button onClick={() => setShowDetailsProperty(null)} className='button1'>Hide Details</button>
-                        </div>
-                    ) : (
-                        <button onClick={() => setShowDetailsProperty(property.id)}>More Details</button>
-                    )}
-                    <p className="property-price">{property.price}</p>
-                    <p className="property-owner">Owner: {property.owner}</p>
+        <div>
+            <header className="home-header">
+                <h1>Rent Hub</h1>
+                <nav className="navbar">
+                    <span className="nav-item"><Link to="/">Home</Link></span>
+                    <span className="nav-item"><Link to="/propertiesforsale">Buy</Link></span>
+                    <span className="nav-item"><Link to="/propertiesforrent">Rent</Link></span>
+                    <span className="nav-item"><Link to="/properties">Properties</Link></span>
+                    <span className="nav-item"><Link to="/login">Login</Link></span>
+                    <span className="nav-item"><Link to="/signup">Sign Up</Link></span>
+                </nav>
+            </header>
+            <div className="property-container">
 
-                    {isPropertyOwner ? (
-                        <div>
-                            <button className='button1'>List Property for Sale</button>
-                            <button className='button1'>List Property for Rent</button>
-                        </div>
-                    ) : (
-                        <div>
-                            <button className='button1'>Buy</button>
-                            <button className='button1' >Rent</button>
-                            <button className='button1'>Request a tour</button>
-                        </div>
-                    )}
+
+                <div className="search-container">
+                    <input
+                        type="text"
+                        placeholder={`Search ${type} properties by title...`}
+                        value={searchTerm}
+                        onChange={(e) => setSearchTerm(e.target.value)}
+                    />
                 </div>
-            ))}
+                {filteredProperties.map((property) => (
+                    <div className="property-card" key={property.id}>
+                        <img
+                            src={property.image}
+                            alt={property.title}
+                            className="property-image"
+                            onError={handleImageError}
+                        />
+                        <h3 className="property-title">{property.title}</h3>
+                        {showDetailsProperty === property.id ? (
+                            <div>
+                                <p className="property-description">{property.description}</p>
+                                <button onClick={() => setShowDetailsProperty(null)} className='button1'>Hide Details</button>
+                            </div>
+                        ) : (
+                            <button onClick={() => setShowDetailsProperty(property.id)}>More Details</button>
+                        )}
+                        <p className="property-price">{property.price}</p>
+                        <p className="property-owner">Owner: {property.owner}</p>
+
+                        {isPropertyOwner ? (
+                            <div>
+                                <button className='button1'>List Property for Sale</button>
+                                <button className='button1'>List Property for Rent</button>
+                            </div>
+                        ) : (
+                            <div>
+                                <button className='button1'>Buy</button>
+                                <button className='button1' >Rent</button>
+                                <button className='button1'>Request a tour</button>
+                            </div>
+                        )}
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
